2b09464fb764070c86d9765dd6e13fdcb4e29550
liu1435533520 <527172332@qq.com>
2021-01-06 17:44:51 +0800

    first commit

---
+++ spider/vue.config.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2021-01-06 15:09:06
+ * @LastEditTime: 2021-01-06 15:55:07
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \electron-demo\spider\vue.config.js
+ */
+module.exports = {
+    pluginOptions: {
+        electronBuilder: {
+            nodeIntegration: true
+        }
+    }
+}
